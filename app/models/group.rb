d923c530f5c194e291f12e7db79a7efec167f11b
nieyicheng <nieyicheng1990@gmail.com>
2016-07-19 17:28:35 +0800

    error

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
-class Group < ActiveRecord::Base
+class User < ApplicationRecord
   belongs_to :user
   has_many :posts
   validates :title, presence: true
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 end
